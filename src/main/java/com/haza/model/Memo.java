ac43837535d2c61aca836c9756f6638bb39bd5a7
jihyeon <kth083030@naver.com>
2023-12-28 19:06:13 +0900

    메모테이블생성

---
+++ src/main/java/com/haza/model/Memo.java
@@ -0,0 +1,32 @@
+package com.haza.model;
+
+import java.security.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Memo {
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int memoId; // 메모번호
+
+	private String title; // 메모 제목
+	private String content; // 메모 내용
+
+	@CreationTimestamp
+	private Timestamp createDate; // 메모 작성일
+
+	@ManyToOne
+	private MemoUser user; // 메모를 작성한 사용자
+
+}
